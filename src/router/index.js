02f36a327f20b0ce08852fde4402e03d09c4f8d8
devsanthoshmk <connectwithsanthoshmk@gmail.com>
2025-09-15 00:38:41 +0530

    added main aboutus updated hero section

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('@/views/AboutViewTemp.vue'),
+      component: () => import('@/views/AboutView.vue'),
     },
 
     // // Shop redirects
